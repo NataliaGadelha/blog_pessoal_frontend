1c04c707bb5e177cd97f61ce678fcab39928fb37
NataliaGadelha <natt.gdl25@gmail.com>
2025-07-31 10:05:12 -0300

    style: alteração da estilização do CardTemas

--- src/components/temas/cardtemas/CardTemas.tsx
+++ src/components/temas/cardtemas/CardTemas.tsx
@@ -1,34 +1,46 @@
-import { Link } from 'react-router-dom'
-import type Tema from '../../../models/Tema'
+import { Link } from "react-router-dom";
+import type Tema from "../../../models/Tema";
+import { Bookmarks, PencilLine, Trash } from "@phosphor-icons/react";
 
-interface CardTemasProps{
-    tema: Tema
+interface CardTemasProps {
+  tema: Tema;
 }
 
 function CardTemas({ tema }: CardTemasProps) {
-    return (
-        <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
-            <header className='py-2 px-6 bg-emerald text-white font-bold text-2xl'>
-                Tema
-            </header>
-            <p className='p-8 bg-emerald-50 text-3xl  h-full'>{tema.descricao}</p>
-            
-            <div className="flex">
-                <Link to={`/editartema/${tema.id}`} 
-                    className='w-full text-slate-100 bg-ligth-green
-                           hover:bg-emerald 
-                        flex items-center justify-center py-2'>
-                    <button>Editar</button>
-                </Link>
+  return (
+    <div className="flex flex-col justify-between rounded-2xl overflow-hidden shadow-lg border border-gray-100 bg-white">
+      <header className="flex items-center gap-45 py-3 px-6 text-gray-500 font-bold text-2xl">
+        Tema
+        <Bookmarks size={32} weight="light" />
+      </header>
+        <hr className="border-slate-300 mx-4" />
+      <div className="p-5 flex-1">
+        <p className="text-xl text-gray-500">{tema.descricao || "Sem descrição"}</p>
+      </div>
 
-                <Link to={`/deletartema/${tema.id}`} className='text-slate-100 bg-red-400 hover:bg-red-700 w-full 
-                    flex items-center justify-center'>
-                    <button>Deletar</button>
-                </Link>
-            </div>
+      <div className="flex gap-3 px-4 pb-4 pt-2">
+        <Link to={`/editartema/${tema.id}`} className="flex-grow">
+          <button
+            type="button"
+            className="w-full flex items-center justify-center gap-5 py-1.5 rounded-lg font-medium text-white bg-gradient-to-r from-teal-400 to-ligth-green hover:from-ligth-green hover:to-teal-400 transition-all shadow-sm"
+          >
+            Editar
+            <PencilLine size={20} weight="light" />
+          </button>
+        </Link>
 
-        </div>
-    )
+        <Link to={`/deletartema/${tema.id}`} className="flex-grow">
+          <button
+            type="button"
+            className="w-full flex  justify-center gap-2 py-1.5 rounded-lg font-medium text-white bg-gradient-to-r from-rose-500 to-red-600 hover:brightness-90 transition-all shadow-sm"
+          >
+            Deletar
+            <Trash size={20} weight="regular" />
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default CardTemas
+export default CardTemas;
